9f5a48da216fbd4acb6ac439c25f92db6927420f
Dhaval Powar 71200 <poward@poward-m01.vmware.com>
2017-04-12 12:14:51 -0700

    InsertionSort + k-Smallest in BST

--- algorithms/sorts/InsertionSort.java
+++ algorithms/sorts/InsertionSort.java
@@ -1,28 +1,36 @@
 import java.util.Arrays;
 
 public class InsertionSort {
-	public static void sort(int [] arr) {
-		if(arr.length == 0 || arr.length == 1)
-			return;
-		int j;
-		for(int i = 0; i < arr.length-1; i++) {
-			
-			j = i+1;
+	public static void sort(int[] arr) {
+		// Edge cases
+		if (arr.length == 0 || arr.length == 1) return;
 
-			while( j > 0 && arr[j] < arr[j-1]) {
-				swap(j, j-1, arr);
-				j--;
+		// Initialize index and temp variables
+		int j = 0, temp = arr[0];
+
+		// Loop over all elements in array
+		for (int i = 1; i < arr.length; i++) {
+			j = i;
+
+			System.out.println(Arrays.toString(arr));
+			// Iterate backwards until 0, to check if current element is in the right place.
+			// If its previous element is greater, swap
+			while (j > 0 && arr[j] < arr[j - 1]) {
+
+				// Swap jth and (j-1)th element
+				temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+				j = j - 1;
 			}
 		}
 	}
 
-	public static void swap(int i, int j, int [] arr) {
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-
-	public static void print(int [] arr) {
+	public static void main (String [] args) {
+		int[] arr = new int [args.length];
+		for (int i = 0; i < args.length; i++)
+			arr[i] = Integer.parseInt(args[i]);
+		sort(arr);
 		System.out.println(Arrays.toString(arr));
 	}
 }
