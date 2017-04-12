d0efb6710745b7f429d142fe384f4e7ef50974f8
Dhaval Powar <poward@poward-m01.vmware.com>
2017-04-11 19:22:58 -0700

    BST - Greatest Common Ancestor

--- data_structures/BinarySearchTree.java
+++ data_structures/BinarySearchTree.java
@@ -18,7 +18,7 @@ public class BinarySearchTree {
     }
 
     public boolean remove(int value) {
-        if(root == null) {
+        if (root == null) {
             return false;
         }
         else if (value == root.getValue()) {
@@ -31,6 +31,34 @@ public class BinarySearchTree {
         else {
             return root.remove(value, null);
         }
+    }
+
+    public int greatestCommonAncestor(int a, int b) {
+        return gcaHelper(a, b, root);
+    }
+
+    public int gcaHelper(int a, int b, BinaryNode temp) {
+        if (a < temp.getValue() && b < temp.getValue()) {
+            return gcaHelper(a, b, temp.getLeft());
+        }
+        else if (a > temp.getValue() && b > temp.getValue()) {
+            return gcaHelper(a, b, temp.getRight());
+        }
+        else {
+            return temp.getValue();
+        }
+    }
+
+    public int greatestCommonAncestorIter(int a, int b) {
+
+        BinaryNode temp = root;
+
+        while ((a < temp.getValue() && b < temp.getValue()) || (a > temp.getValue() && b > temp.getValue())) {
+            if (a < temp.getValue() && b < temp.getValue()) temp = temp.getLeft();
+            if (a > temp.getValue() && b > temp.getValue()) temp = temp.getRight();
+        }
+
+        return temp.getValue();
     }
 
     public void print() {
