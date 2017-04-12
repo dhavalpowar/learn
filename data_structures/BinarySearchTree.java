1987e7aeaa93908d2e451d859ffc12a4b75859d5
Dhaval Powar <poward@poward-m01.vmware.com>
2017-04-12 12:14:51 -0700

    InsertionSort + k-Smallest in BST

--- data_structures/BinarySearchTree.java
+++ data_structures/BinarySearchTree.java
@@ -2,6 +2,24 @@ package learn.data_structures;
 
 public class BinarySearchTree {
 
+    /**
+     * Inner class that holds a pair of sum and count
+     *
+     */
+    public class SumCountPair {
+        int sum, count;
+
+        public SumCountPair(int k) {
+            sum = 0;
+            this.count = k;
+        }
+
+        public SumCountPair(int sum, int count) {
+            this.sum = sum;
+            this.count = count;
+        }
+    }
+
     BinaryNode root;
 
     public BinarySearchTree(int value) {
@@ -33,11 +51,28 @@ public class BinarySearchTree {
         }
     }
 
-    public int greatestCommonAncestor(int a, int b) {
-        return gcaHelper(a, b, root);
+    public int sumOfSmallest(int k) {
+        SumCountPair p = new SumCountPair(k);
+        sumOfSmallest(p, root);
+        return p.sum;
     }
 
-    public int gcaHelper(int a, int b, BinaryNode temp) {
+    private void sumOfSmallest(SumCountPair p, BinaryNode temp) {
+        if(temp.getLeft() != null) sumOfSmallest(p, temp.getLeft());
+        if(p.count == 0) {
+            return;
+        } else {
+            p.sum = p.sum + temp.getValue();
+            p.count = p.count - 1;
+        }
+        if(temp.getRight() != null) sumOfSmallest(p, temp.getRight());
+    }
+
+    public int greatestCommonAncestor(int a, int b) {
+        return gcaHelper(a, b, root).getValue();
+    }
+
+    private BinaryNode gcaHelper(int a, int b, BinaryNode temp) {
         if (a < temp.getValue() && b < temp.getValue()) {
             return gcaHelper(a, b, temp.getLeft());
         }
@@ -45,7 +80,7 @@ public class BinarySearchTree {
             return gcaHelper(a, b, temp.getRight());
         }
         else {
-            return temp.getValue();
+            return temp;
         }
     }
 
@@ -61,6 +96,39 @@ public class BinarySearchTree {
         return temp.getValue();
     }
 
+    /**
+     * Finds the max integer (for a path) between 2 nodes.
+     * Soln:- Find GCA and then max(max(gca,a), max(gca, b))
+     *
+     */
+    public int maximElementBetween2Nodes(int a, int b) {
+        BinaryNode gca = gcaHelper(a, b, root);
+        return Integer.max(
+            findMaxBetweenAncestorAndDescendant(gca, a),
+            findMaxBetweenAncestorAndDescendant(gca, b)
+        );
+    }
+
+    /**
+     * Finds the max between an ancestor and a descendant.
+     *
+     */
+    private int findMaxBetweenAncestorAndDescendant(BinaryNode gca, int n) {
+        int largest = Integer.MIN_VALUE;
+        BinaryNode temp = gca;
+        while (temp.getValue() != n) {
+            int current = temp.getValue();
+            largest = current > largest ? current : largest;
+            if (current > n) {
+                temp = temp.getLeft();
+            }
+            else {
+                temp = temp.getRight();
+            }
+        }
+        return largest;
+    }
+
     public void print() {
         root.printInOrder();
     }
@@ -70,8 +138,10 @@ public class BinarySearchTree {
         for(int i = 1; i < args.length; i++) {
             bst.insert(Integer.parseInt(args[i]));
         }
+        
         bst.print();
         System.out.println(bst.remove(12));
         bst.print();
+        System.out.println(bst.sumOfSmallest(3));
     }
 }
